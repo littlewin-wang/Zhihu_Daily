49db5100d4b951d9b0c116e88a4cf41a7f245d0e
littlewin <littlewin.wang@gmail.com>
2016-12-27 19:18:22 +0800

    Update

---
+++ src/components/common/header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="head">
+    <h1>
+      <em></em>
+      <router-link tag="a" to="/">知乎日报</router-link>
+    </h1>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
