75fb728fcf55fd9078dd83fb9e4bef64067214aa
littlewin <littlewin.wang@gmail.com>
2017-01-09 13:10:45 +0800

    update

--- prod.server.js
+++ prod.server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var config = require('./config')
 var proxyMiddleware = require('http-proxy-middleware')
 
-var port = process.env.PORT
+var port = process.env.PORT || config.build.port
 var proxyTable = config.dev.proxyTable
 
 var app = express()
